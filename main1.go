d63f2cf78a4da2cb513985fd0442a89312f59c0a
vortex <vortex@localhost.localdomain>
2020-02-11 12:31:28 +0530

    changes in test

--- main1.go
+++ main1.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/nidhish1/test"
 )
 
 
 func main() {
 	fmt.Println("sdf")
+	test.foo()
 
 } 
--- test/test.go
+++ test/test.go
@@ -4,6 +4,6 @@ import  (
 	"fmt"
 )
 
-func foo () {
+func Foo () {
 	fmt.Println("sdf")
 }
